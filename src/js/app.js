28e92650cd497446b614183a232306db75bb1725
Krista Anne Nordgren <kristaanne9@MacBook-Pro-8.local>
2017-07-02 20:58:22 -0400

    new stuff

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,13 @@
 // The namespace for this application
-var app = {};
+// ()
+function setup() {
+  var myCanvas = createCanvas(200, 400);
+  myCanvas.parent('animation-1');
+}
+
+function draw() {
+   stroke(map(mouseX, 0, windowWidth, 0, 255), 100, 255);
+   fill(176, 176, 176);
+   ellipse(map(mouseX, 0, 200, 0, 200), map(mouseY, 0, 400, 0, 400), 50, 50);
+ 
+}
