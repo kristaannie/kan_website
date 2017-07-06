667fe2698cabe963c98b7e39a7d52837febae789
Krista Anne Nordgren <kristaanne9@MacBook-Pro-8.local>
2017-07-05 23:52:45 -0400

    module reorder and sketch 3

--- src/js/animation_2.js
+++ src/js/animation_2.js
@@ -2,15 +2,33 @@
 // ()
 var sketch2 = function(p) {
 
+  p.i = 0;
+  p.j = 0;
+
+  
+
+
   p.setup = function() {
     var myCanvas2 = p.createCanvas(200, 400);
     myCanvas2.parent('animation-2');
+    // p.frameRate(1);
+    
   }
 
   p.draw = function() {
      p.stroke(62, 130, 255);
      p.strokeWeight(2);
      p.fill(176, 176, 176);
+
+    // var scatter_width = p.random(200);
+    // var scatter_height = p.random(400);
+
+    // for (p.i = 0; p.i < 200; p.i += + 10){
+    //   for (p.j = 0; p.j < 100; p.j += + 10) {
+    //     p.rect(p.i, p.j, 50, 50);
+    //   }
+    // }
+     
      p.rect(p.map(p.mouseX, 0, 200, 0, 200), p.map(p.mouseY, 0, 400, 0, 400), 50, 50);
  
   }
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,10 @@
 
 var sketch1 = function(p) {
 
+  p.i = 0;
+  p.j = 0;
+
+
   p.setup = function() {
     var myCanvas2 = p.createCanvas(200, 400);
     myCanvas2.parent('animation-1');
@@ -12,6 +16,13 @@ var sketch1 = function(p) {
      p.stroke(62, 130, 255);
      p.strokeWeight(2);
      p.fill(176, 176, 176);
+
+    //  for (p.i = 0; p.i < 200; p.i += + 10){
+    //   for (p.j = 0; p.j < 100; p.j += + 10) {
+    //     p.ellipse(p.i, p.j, 50, 50);
+    //   }
+    // }
+
      p.ellipse(p.map(p.mouseX, 0, 200, 0, 200), p.map(p.mouseY, 0, 400, 0, 400), 50, 50);
  
   }
